eced414c78417977b0f17cda2c36689f0a7788d9
jbigalet <joran.bigalet@gmail.com>
2015-12-09 00:39:41 +0100

    get peers from tracker

--- client.jl
+++ client.jl
@@ -1,8 +1,9 @@
-module yabc
+#module yabc
 
 include("bdecode.jl")
 include("bencode.jl")
 using SHA
+using HTTPClient
 
 # poor man's urlencode
 hex2url(h) = join(["%$(h[i:i+1])" for i in 1:2:length(h)])
@@ -11,7 +12,14 @@ function metainfo(filepath)
     meta = BDecode.bdecode(readall(filepath))
     info_hash = sha1(BEncode.bencode(meta["info"]))
     tracker_url = "$(meta["announce"])?info_hash=$(hex2url(info_hash))&peer_id=jklmfdsqjklmfdsqjklm&port=6888&uploaded=0&downloaded=0&left=$(meta["info"]["piece length"])&compact=1&no_peer_id=1&event=started"
-    return {"meta" => meta, "info_hash" => info_hash, "tracker_url" => tracker_url}
+    return Dict{AbstractString,Any}("meta" => meta, "info_hash" => info_hash, "tracker_url" => tracker_url)
 end
 
-end
+meta = metainfo("examples\\archlinux.torrent")
+resp = BDecode.bdecode(bytestring(HTTPClient.get(meta["tracker_url"]).body))
+
+# parse peers
+peers = readbytes(IOBuffer(resp["peers"]))
+peerlist = [(peers[i:i+3],peers[i+4:i+5]) for i in 1:6:sizeof(peers)]
+
+#end
